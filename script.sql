e062af70f33988864e158a0f403310e0e6688198
Prashanti <67358618+Prashanti02@users.noreply.github.com>
2021-07-11 14:05:35 +0700

    Update script.sql

--- script.sql
+++ script.sql
@@ -13,7 +13,6 @@ CREATE TABLE Delegates
   CommitteeN VARCHAR(50) NOT NULL,
   Group VARCHAR(10),
   DelName VARCHAR(100) NOT NULL,
-  ID INT NOT NULL,
   PRIMARY KEY (DelegateID)
 );
 
@@ -25,7 +24,6 @@ CREATE TABLE Pdetails
   Visareq VARCHAR(20),
   NO INT NOT NULL,
   IDS INT NOT NULL,
-  DelegateID INT NOT NULL,
   PRIMARY KEY (NO)
 );
 
@@ -35,16 +33,23 @@ CREATE TABLE Dais_
   Position VARCHAR(50) NOT NULL,
   Committee VARCHAR(50) NOT NULL,
   Dname VARCHAR(100) NOT NULL,
-  IDS INT NOT NULL,
   PRIMARY KEY (DaisID)
 );
 
-CREATE TABLE Is_assigned_
-(
-  DelegateID INT NOT NULL,
-  DaisID INT NOT NULL,
-  PRIMARY KEY (DelegateID, DaisID),
-  FOREIGN KEY (DelegateID) REFERENCES Delegates(DelegateID),
-  FOREIGN KEY (DaisID) REFERENCES Dais_(DaisID)
-);
+
+
+
+INSERT INTO `delegates`(`delegateID`, `delName`, `committeeN`, `assignedCountry`, `group`) VALUES (2103,"Heather Mouuse","SOCHUM","USA","N")
+INSERT INTO `delegates`(`delegateID`, `delName`, `committeeN`, `assignedCountry`, `group`) VALUES (2104,"Jason K","UNDP","Nepal","N")
+
+INSERT INTO `dais`(`daisID`, `dName`, `committee`, `position`) VALUES (1105,"Richard Sean","SOCHUM","Vice-chair")
+INSERT INTO `dais`(`daisID`, `dName`, `committee`, `position`) VALUES (1106,"Samantha Paul","UNHCR","Vice-chair")
+
+INSERT INTO `organizingcommittee`(`ID`, `name`, `role`, `responsibility`) VALUES (4, "Luke M", "Deelgate Affairs", "Certificates management")
+INSERT INTO `organizingcommittee`(`ID`, `name`, `role`, `responsibility`) VALUES (5, "Logan H", "Delegate Affairs", "Closing")
+
+INSERT INTO `pdetails`(`NO`, `Flight`, `Room`, `Visareq`, `Country`, `IDS`) VALUES (2,"QR431","A235","Y","Germany","1102")
+INSERT INTO `pdetails`(`NO`, `Flight`, `Room`, `Visareq`, `Country`, `IDS`) VALUES (3,"EI567","A437","Y","France","1103")
+
+
 
