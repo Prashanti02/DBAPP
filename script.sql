e2f0826a58bef1172e55e236f338cdcbfff30d8b
Prashanti <67358618+Prashanti02@users.noreply.github.com>
2021-07-17 12:16:16 +0700

    Update script.sql

--- script.sql
+++ script.sql
@@ -9,19 +9,19 @@ CREATE TABLE organizingcommittee
 
 CREATE TABLE delegates
 (
-  delegateID INT NOT NULL,
+  delegateID VARCHAR(50) NOT NULL,
   delName VARCHAR(50) NOT NULL,
   committeeN VARCHAR(100) NOT NULL,
   assignedCountry VARCHAR(50) NOT NULL,
   ID INT NOT NULL,
   PRIMARY KEY (delegateID),
- FOREIGN KEY (ID) REFERENCES organizingcommittee (ID)
+FOREIGN KEY (ID) REFERENCES organizingcommittee (ID)
   );
 
 CREATE TABLE dais
 (
-  daisID INT NOT NULL,
-  dname VARCHAR(100) NOT NULL,
+  daisID VARCHAR (50)  NOT NULL,
+  dName VARCHAR(100) NOT NULL,
   committee VARCHAR(50) NOT NULL,
   position VARCHAR(50) NOT NULL,
   ID INT NOT NULL,
@@ -36,7 +36,7 @@ CREATE TABLE pdetails
   Room VARCHAR(20) NOT NULL,
   Visareq VARCHAR(20) NOT NULL,
   Country VARCHAR(50) NOT NULL,
-  IDS INT NOT NULL,
+  IDS VARCHAR(50),
   PRIMARY KEY (NO),
 FOREIGN KEY (IDS) REFERENCES delegates (delegateID)
 );
@@ -44,17 +44,19 @@ FOREIGN KEY (IDS) REFERENCES delegates (delegateID)
 
 
 
-INSERT INTO `delegates`(`delegateID`, `delName`, `committeeN`, `assignedCountry`, `ID`) VALUES (2103,"Heather Mouuse","SOCHUM","USA", 4);
-INSERT INTO `delegates`(`delegateID`, `delName`, `committeeN`, `assignedCountry`, `ID`) VALUES (2104,"Jason K","UNDP","Brazil", 5);
+INSERT INTO `organizingcommittee`(`ID`, `name`, `role`, `responsibility`) VALUES (4, "Luke M", "Deelgate Affairs", "Manage delegates");
+INSERT INTO `organizingcommittee`(`ID`, `name`, `role`, `responsibility`) VALUES (5, "Logan H", "Delegate Affairs", "Awards");
+
+
+INSERT INTO `delegates`(`delegateID`, `delName`, `committeeN`, `assignedCountry`, `ID`) VALUES (2103,"Heather Mouuse","SOCHUM","USA", 5);
+INSERT INTO `delegates`(`delegateID`, `delName`, `committeeN`, `assignedCountry`, `ID`) VALUES (2104,"Jason K","UNDP","Brazil", 4);
 
 INSERT INTO `dais`(`daisID`, `dName`, `committee`, `position`, `ID`) VALUES (1105,"Richard Sean","SOCHUM","Chair", 4);
 INSERT INTO `dais`(`daisID`, `dName`, `committee`, `position`, `ID`) VALUES (1106,"Samantha Paul","UNHCR", "Vice-chair", 4);
 
-INSERT INTO `organizingcommittee`(`ID`, `name`, `role`, `responsibility`) VALUES (4, "Luke M", "Deelgate Affairs", "Manage delegates");
-INSERT INTO `organizingcommittee`(`ID`, `name`, `role`, `responsibility`) VALUES (5, "Logan H", "Delegate Affairs", "Awards");
 
-INSERT INTO `pdetails`(`NO`, `Flight`, `Room`,  `Visareq`, `Country`, `IDS`) VALUES (1,"QR431","A235","Y", "Switzerland", 2103);
-INSERT INTO `pdetails`(`NO`, `Flight`, `Room`,  `Visareq`, `Country`, `IDS`) VALUES (2,"QR431","A457","Y", "Germany", 2104);
+INSERT INTO `pdetails`(`NO`, `Flight`, `Room`,  `Visareq`, `Country`, `IDS`) VALUES (1,"QR431","A235","Y", "Switzerland", "2103");
+INSERT INTO `pdetails`(`NO`, `Flight`, `Room`,  `Visareq`, `Country`, `IDS`) VALUES (2,"QR431","A457","Y", "Germany", "2104");
 
 
 
